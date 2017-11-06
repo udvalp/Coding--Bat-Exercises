1893778f49cea6d517cb1341c07a718ae636c1da
Udval Panchlothia <udval.ce@gmail.com>
2017-11-06 11:23:49 -0800

    w

---
+++ Strings3.java
@@ -0,0 +1,20 @@
+
+public class Strings3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(sameEnds("TESTendyyyET"));
+	
+	}
+	
+	
+	static public String sameEnds(String string) {
+		if(string.length()<2)return string;
+		
+		
+		return string;
+	}
+
+
+}
