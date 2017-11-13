5a997e9e181b94cc34b05d0ae157caec2d839b20
Udval Panchlothia <udval.ce@gmail.com>
2017-11-12 22:37:33 -0800

    Solved

--- Strings3.java
+++ Strings3.java
@@ -3,44 +3,45 @@ public class Strings3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println(sameEnds("TESTendyyyET"));
-	
+
 	}
-	
-	
+
+
 	static public String sameEnds(String string) {
 		if(string.length()<2)return string;
-		
+
 		return string;
 	}
-	
-	
 
-/*	Given a string and an int n, return a string made of n repetitions of the last n 
- * characters of the string. You may assume that n is between 0 and the
- *  length of the string, inclusive.
-*/	public String repeatEnd(String str, int n) {
-		String retString ='';
-		for(int i=0;i<n;i++) {
-			
-			retString+=str.substring(str.length()-n);
-		}
-		
-		return retString;
-		  
-	}
-	
-	public boolean prefixAgain(String str, int n) {
-		  if(str.length()<n)return false;
-			
-			String subStr = str.substring(0,n);
-			if(str.indexOf(subStr,n)>0)
-				return true;
-			else 
-				return false;
-		  
-	}
+
+
+	/*
+	 * Given a string and an int n, return a string made of n repetitions of the
+	 * last n characters of the string. You may assume that n is between 0 and the
+	 * length of the string, inclusive.
+	 */ public String repeatEnd(String str, int n) {
+		 String retString = "";
+		 for (int i = 0; i < n; i++) {
+
+			 retString += str.substring(str.length() - n);
+		 }
+
+		 return retString;
+
+	 }
+
+	 public boolean prefixAgain(String str, int n) {
+		 if(str.length()<n)return false;
+
+		 String subStr = str.substring(0,n);
+		 if(str.indexOf(subStr,n)>0)
+			 return true;
+		 else 
+			 return false;
+
+	 }
 
 
 
