eacc057e8da238202af3d191049d0d669ff036d0
Udval Panchlothia <udval.ce@gmail.com>
2017-11-12 22:35:58 -0800

    Trial 1

--- Strings3.java
+++ Strings3.java
@@ -17,8 +17,17 @@ public class Strings3 {
 	
 	
 
-/*	Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
+/*	Given a string and an int n, return a string made of n repetitions of the last n 
+ * characters of the string. You may assume that n is between 0 and the
+ *  length of the string, inclusive.
 */	public String repeatEnd(String str, int n) {
+		String retString ='';
+		for(int i=0;i<n;i++) {
+			
+			retString+=str.substring(str.length()-n);
+		}
+		
+		return retString;
 		  
 	}
 	
